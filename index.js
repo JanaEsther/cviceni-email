8f6fc5c0f62f780dfdc1d182f206842793977656
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-08-29 21:26:22 +0200

    fillBody+fillSubject

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const goodbye = (name) => {
+  const closingElement = document.querySelector('.email__closing');
+  closingElement.textContent = `S pozdravem ${name} `;
+};
 
+goodbye('Jana Obendraufová');
+
+const fillSubject = (subject) => {
+  const emailElement = document.querySelector('.email__subject');
+  emailElement.textContent = `${subject}`;
+};
+
+const fillBody = (body) => {
+  const bodyElement = document.querySelector('.email__body');
+  bodyElement.textContent = `${body}`;
+};
+
+fillSubject('Pracovní nabídka');
+fillBody(
+  'reaguji na Vaši nabídku, s tím, že opravdu není o co stát. Pokud někdo přesto projeví zájem, tak se na něj přijdu osobně podívat.Takového troubu aby pohledal.',
+);
